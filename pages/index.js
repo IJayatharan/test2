3ab73ee995b7f4b1eea2cefc614d2d0371e5b14a
IJayatharan <indran@calcey.com>
2022-10-19 03:14:40 +0000

    build-45-commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World - Deployed from jenkins
+          Hello World - Test Both
         </h1>
 
         <p className={styles.description}>
