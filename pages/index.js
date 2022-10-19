8e82d146531428b54d83b5495805206cdfb3bae6
IJayatharan <indran@calcey.com>
2022-10-19 04:45:46 +0000

    build-51-commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World - Test All
+          Hello World - Test GitHub
         </h1>
 
         <p className={styles.description}>
